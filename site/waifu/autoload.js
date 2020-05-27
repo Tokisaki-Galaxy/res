a32aa916bf2b06363ce0a8b89deb61e568ca64aa
Tokisaki-Galaxy <tokisaki-galaxy@qq.com>
2020-05-27 23:06:38 +0800

    Des: Upd:2020/05/27 23:06:38.12

--- site/waifu/autoload.js
+++ site/waifu/autoload.js
@@ -1,4 +1,4 @@
-try {
+//try {
     $("<link>").attr({href: "https://cdn.jsdelivr.net/gh/Tokisaki-Galaxy/res/site/waifu/waifu.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
     $.ajax({url: 'https://cdn.jsdelivr.net/gh/Tokisaki-Galaxy/res/site/waifu/waifu-tips.min.js',dataType:"script", cache: true, async: false});
@@ -21,4 +21,4 @@ try {
     live2d_settings['homePageUrl'] = '<%- config.url %>';
     /* 在 initModel 前添加 */
     initModel('https://cdn.jsdelivr.net/gh/Tokisaki-Galaxy/res/site/waifu/waifu-tips.json');
-} catch(err) { console.log('[Error] JQuery is not defined.') }
+//} catch(err) { console.log('[Error] JQuery is not defined.') }
