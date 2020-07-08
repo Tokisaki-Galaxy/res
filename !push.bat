573f206d06c749bd9dbb3c5ab31a03b60b5d4931
Tokisaki-Galaxy <tokisaki-galaxy@qq.com>
2020-07-08 22:31:31 +0800

    Upd:2020/07/08 22:31:31.44

--- !push.bat
+++ !push.bat
@@ -1,6 +1,4 @@
 @echo off
-echo Enter a description of this push(Optional):
-set /p d=
 git add -A
-git commit -m "Des:%d% Upd:%date:~0,4%/%date:~5,2%/%date:~8,2% %time%"
+git commit -m "Upd:%date:~0,4%/%date:~5,2%/%date:~8,2% %time%"
 git push||git push||git push
